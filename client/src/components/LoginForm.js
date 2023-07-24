3ee5a505be925ccdabbdcf338935cffe5c29d268
matthewmontiel <xoptimuszerox@gmail.com>
2023-07-23 19:47:29 -0500

    fixed typo in login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,4 +1,4 @@
-mport React, { useState } from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 
